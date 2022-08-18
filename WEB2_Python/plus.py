981fd7c0f20f29449bbe160722db280a201b4b8c
tree4553 <tree4553@naver.com>
2022-08-19 08:25:09 +0900

    WEB2_Python

--- WEB2_Python/plus.py
+++ WEB2_Python/plus.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!C:\Users\LJC\AppData\Local\Programs\Python\Python38-32\python.exe
 print("content-type:text,html; charset=UTF-8\n")
 print(1+1)
--- WEB2_Python/syntax/number.py
+++ WEB2_Python/syntax/number.py
@@ -1,5 +1,4 @@
 #!C:\Users\LJC\AppData\Local\Programs\Python\Python38-32\python.exe
 print("content-type:text,html; charset=UTF-8\n")
-print("Hello World!")
-text = 'window webserver'
-print(text)
+print(1+1)
+print('https://docs.python.org/3/library/stdtypes.html')
