f25930e9759fa17ba0fb6f10e7dc0ab07bd63034
tree4553 <tree4553@naver.com>
2022-08-20 18:25:23 +0900

    WEB2_Python

--- WEB2_Python/index.py
+++ WEB2_Python/index.py
@@ -4,25 +4,28 @@ print()
 
 import cgi
 form = cgi.FieldStorage()
-pageId = form["id"].value
-print(pageId)
-
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/' + pageId, 'r', encoding="utf-8").read()
+else:
+    pageId = 'Welcome'
+    description = 'Hello, web'
 print('''<!doctype html>
 <html>
 <head>
-  <title>WEB1 - Welcome</title>
-  <meta charset="utf-8">
+    <title>WEB1 - Welcome</title>
+    <meta charset="utf-8">
 </head>
 <body>
-  <h1><a href="index.html">WEB</a></h1>
-  <ol>
-    <li><a href="index.py?id=HTML">HTML</a></li>
-    <li><a href="index.py?id=CSS">CSS</a></li>
-    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>{title}</h2>
-  <p>The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
-  </p>
+    <h1><a href="index.py">WEB</a></h1>
+    <ol>
+        <li><a href="index.py?id=HTML">HTML</a></li>
+        <li><a href="index.py?id=CSS">CSS</a></li>
+        <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+        <li><a href="index.py?id=Python">Python</a></li>
+    </ol>
+    <h2>{title}</h2>
+    <p>{desc}</p>
 </body>
 </html>
-'''.format(title=pageId))
+'''.format(title=pageId, desc=description))
---
+++ WEB2_Python/syntax/conditional.py
@@ -0,0 +1,25 @@
+user_id = input('id? ')
+user_pwd = input('password? ')
+'''
+if user_id == '111111':
+    print('Hello master')
+else:
+    print('Who are you?')
+'''
+
+'''
+if user_id == 'egoing':
+    if user_pwd == '111111':
+        print('Hello master')
+    else:
+        print('Who are you?')
+else:
+    print('Who are you?')
+'''
+
+if user_id == 'egoing' and user_pwd == '111111':
+    print('Hello master')
+elif: user_id == 'leezche' and user_pwd == '222222':
+    print('Hello master')
+else:
+    print('Who are you?')
