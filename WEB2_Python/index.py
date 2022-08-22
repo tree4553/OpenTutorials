5f9ee4ca3ccce8e2706d9033b9728ebf8115c4b1
tree4553 <tree4553@naver.com>
2022-08-22 16:31:07 +0900

    WEB2_Python

--- WEB2_Python/index.py
+++ WEB2_Python/index.py
@@ -2,14 +2,17 @@
 print("Content-Type: text/html")
 print()
 
-import cgi, os, view
+import cgi, os, view, html_sanitizer
+sanitizer = html_sanitizer.Sanitizer()
 
 form = cgi.FieldStorage()
 if 'id' in form:
-    pageId = form["id"].value
+    title = pageId = form["id"].value
     description = open('data/' + pageId, 'r', encoding="utf-8").read()
-    description = description.replace('<', '&lt;')
-    description = description.replace('>', '&gt;')
+    #description = description.replace('<', '&lt;')
+    #description = description.replace('>', '&gt;')
+    title = sanitizer.sanitize(title)
+    description = sanitizer.sanitize(description)
     update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
     delete_action = '''
         <form action="process_delete.py" method="post">
@@ -18,7 +21,7 @@ if 'id' in form:
         <form>
     '''.format(pageId)
 else:
-    pageId = 'Welcome'
+    title = pageId = 'Welcome'
     description = 'Hello, web'
     update_link = ''
     delete_action = ''
@@ -47,7 +50,7 @@ print('''<!doctype html>
 </body>
 </html>
 '''.format(
-title=pageId,
+title=title,
 desc=description,
 listStr=view.getList(),
 update_link=update_link,
